29cdddbeb555fea9b3752051928b1537953f66fc
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-24 21:02:47 -0300

    WIP - criado header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <h1>Estrutura Inicial</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

44004859021cfde5f08c40c96caf0611e89abb0c
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-10-31 17:34:39 -0300

    WIP - criando estrutura inicial do componente LoginForm

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-export default function Login() {
-  return <div>Login</div>;
-}
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+
+const Login = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Login;

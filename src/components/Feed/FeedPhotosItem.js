82143a34821ccf32dc300f6077d0c0925d009ce8
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2021-05-30 21:55:52 -0300

    WIP - estilizado esqueleto das imagens a serem carregadas

--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import React from "react";
+import Image from "../Helper/Image";
+import styles from "./FeedPhotosItem.module.css";
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
-
   function handleClick() {
-    setModalPhoto(photo)
+    setModalPhoto(photo);
   }
-  
+
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );

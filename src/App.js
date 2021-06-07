20382ad69d2af23d32766cec2cf77bcf952d64ca
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2021-06-07 18:22:57 -0300

    WIP - exibindo foto por perfil do usuário

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { UserStorage } from "./UserContext";
 import User from "./components/User/User";
 import ProtectedRoute from "./components/Helper/ProtectedRoute";
 import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
             <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
